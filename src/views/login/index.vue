ccfbbe3a454efc974cf4e9b4a21023176f76e1e8
Liwenxiu <742952713@qq.com>
2019-12-19 09:26:16 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,32 +4,65 @@
     <div class="top">
       <img src="../../assets/img/logo_index.png" alt="">
     </div>
-    <!-- 手机号的表单域 -->
-    <el-form>
-      <el-form-item>
-        <el-input placeholder="请输入您的手机号"></el-input>
+    <!-- 表单容器 -->
+    <el-form :model="ruleForm" :rules="rules" ref="hehe">
+      <!-- 手机号的表单域 -->
+      <el-form-item prop="mobile">
+        <el-input v-model="ruleForm.mobile" placeholder="请输入您的手机号"></el-input>
       </el-form-item>
-    </el-form>
-    <!-- 验证码的表单域 -->
-    <el-form>
-      <el-form-item>
-        <el-input placeholder="请输入您收到的验证码" style="width:68%"></el-input>
+      <!-- 验证码的表单域 -->
+      <el-form-item prop="code">
+        <el-input v-model="ruleForm.code" placeholder="请输入您收到的验证码" style="width:68%"></el-input>
         <el-button plain style="float:right">发送验证码</el-button>
       </el-form-item>
-    </el-form>
-    <!-- 复选框的表单域 -->
-    <el-form>
-      <el-form-item>
-        <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+      <!-- 复选框的表单域 -->
+      <el-form-item prop="check">
+        <el-checkbox v-model="ruleForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
       </el-form-item>
+      <!-- 最下面的登录按钮 -->
+      <!-- <el-form-item> -->
+        <el-button type="primary" style="width:100%" @click="myformLogin">登录</el-button>
+      <!-- </el-form-item> -->
     </el-form>
-    <el-button type="primary" style="width:100%">登录</el-button>
   </el-card>
 </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 需要校验的表单数据对象
+      ruleForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      // 需要校验的表单规则
+      rules: {
+        mobile: [{ required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }],
+        code: [{ required: true, message: '请输入您收到的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码不正确' }],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('您还未勾选我们的条款哦~'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    myformLogin () {
+      this.$refs.hehe.validate(function (isOK) {
+        if (isOK) {
+          console.log('表单中的所有数据都校验成功了哦')
+        }
+      })
+    }
+  }
 
 }
 </script>
