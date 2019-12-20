7a03d8a5c330000450c9cc8bbf8546ff2724c9c5
Liwenxiu <742952713@qq.com>
2019-12-20 16:50:13 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,11 @@
 <template>
-<div>
-  主页一级路由
-</div>
+<el-container>
+  <el-aside>左边栏</el-aside>
+  <el-container>
+    <el-header>右侧头部</el-header>
+    <el-main>右侧主体</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
