8e8160275146866a130f82a1d0eb1e4f465f44cf
Liwenxiu <742952713@qq.com>
2019-12-21 01:16:02 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,53 @@
+<template>
+<!-- 根据页面的情况自由组合布局 -->
+<!-- 一行 -->
+<el-row class="layout-header" type="flex" align="middle">
+  <!-- 一行里面有两列 -->
+  <el-col class="left" :span="12">
+    <i class="el-icon-s-fold"></i>
+    <span>江苏传智播客教育科技股份有限公司</span>
+  </el-col>
+  <!-- 右边的这一列里又有一行 -->
+  <el-col class="right" :span="12">
+    <el-row type="flex" justify="end" align="middle">
+      <img src="../../assets/img/abc.jpg" alt="">
+      <el-dropdown>
+        <span>Penni-vii</span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>git地址</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-row>
+  </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+  height: 60px;
+  .left {
+    font-size: 20px;
+    span {
+      font-size: 16px;
+      color: #2c3e50;
+      margin-left: 4px;
+    }
+  }
+  .right {
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin-right: 4px;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,17 @@
 <template>
+<!-- 整个页面的大容器 -->
 <el-container>
-  <el-aside>
-    <layout-aside style="height:100vh;background-color:#353b4e;width:230px;"></layout-aside>
+  <!-- 左侧导航 -->
+  <el-aside style="height:100vh;background-color:#353b4e;width:230px;">
+    <layout-aside></layout-aside>
   </el-aside>
+  <!-- 右侧容器 -->
   <el-container>
-    <el-header>右侧头部</el-header>
+    <!-- 右侧头部 -->
+    <el-header>
+      <layout-header></layout-header>
+    </el-header>
+    <!-- 右侧主体 -->
     <el-main>右侧主体</el-main>
   </el-container>
 </el-container>
@@ -12,9 +19,11 @@
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
