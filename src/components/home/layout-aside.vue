d408bf9f1629284685574a98190585ca7de168ce
Liwenxiu <742952713@qq.com>
2019-12-21 10:27:24 +0800

    黑马头条PC-主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,27 +3,27 @@
     <div class="title">
       <img src="../../assets/img/logo_admin.png" alt="">
     </div>
-    <el-menu background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
-      <el-menu-item index="1">首页</el-menu-item>
-      <el-submenu index="2">
+    <el-menu router background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
+      <el-menu-item index="/home" >首页</el-menu-item>
+      <el-submenu index="1">
         <template slot="title">
           内容管理
         </template>
-        <el-menu-item index="2-1">发布文章</el-menu-item>
-        <el-menu-item index="2-2">文章列表</el-menu-item>
-        <el-menu-item index="2-3">评论管理</el-menu-item>
-        <el-menu-item index="2-4">素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">文章列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论管理</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
       </el-submenu>
-      <el-submenu index="3">
+      <el-submenu index="2">
         <template slot="title">
           粉丝管理
         </template>
-        <el-menu-item index="3-1">图文数据</el-menu-item>
-        <el-menu-item index="3-2">粉丝概况</el-menu-item>
-        <el-menu-item index="3-3">粉丝画像</el-menu-item>
-        <el-menu-item index="3-4">粉丝列表</el-menu-item>
+        <el-menu-item index="/home/picture">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanspicture">粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
       </el-submenu>
-      <el-menu-item index="4">账户信息</el-menu-item>
+      <el-menu-item index="/home/userinfo">账户信息</el-menu-item>
     </el-menu>
   </div>
 </template>
