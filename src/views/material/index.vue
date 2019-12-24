df0f2a99c80cd7c424a8a6e3ddd30ae54652622a
Liwenxiu <742952713@qq.com>
2019-12-24 22:42:53 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,12 +5,20 @@
       素材管理
     </template>
    </bread-crumb>
+   <el-tabs v-model="activeName">
+     <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
+     <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+   </el-tabs>
  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
