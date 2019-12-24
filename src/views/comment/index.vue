2aec1ee60f2ce095af1094a1076310510e1fee9d
Liwenxiu <742952713@qq.com>
2019-12-24 11:10:06 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,6 +1,20 @@
 <template>
+  <!-- 整个是一个卡片 -->
   <el-card>
-    嘻嘻嘻，这是评论管理组件
+    <!-- 用自己封装的面包屑组件 -->
+    <bread-crumb slot="header">
+      <template slot="title">
+        评论管理
+      </template>
+    </bread-crumb>
+    <!-- 表格部分 -->
+    <el-table>
+      <el-table-column label="标题" width="600"></el-table-column>
+      <el-table-column label="评论状态"></el-table-column>
+      <el-table-column label="总评论数"></el-table-column>
+      <el-table-column label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
+    </el-table>
   </el-card>
 </template>
 
