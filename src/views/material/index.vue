551744879fae7d1080d3595944350b9d4b705b1a
Liwenxiu <742952713@qq.com>
2019-12-25 00:24:26 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -19,8 +19,23 @@
          </el-card>
        </div>
      </el-tab-pane>
-     <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+     <el-tab-pane label="收藏图片" name="collect">
+       <div class="hehe">
+         <el-card class="img-card" v-for="item in list" :key="item.id">
+           <img :src="item.url" alt="">
+         </el-card>
+       </div>
+     </el-tab-pane>
    </el-tabs>
+   <!-- 公共分页组件 -->
+   <el-row type="flex" justify="center" height="80px">
+      <el-pagination background layout="prev, pager, next"
+      :total="page.total"
+      :page-size="page.pageSize"
+      :current-page="page.currentPage"
+      @current-change="changePage"
+      ></el-pagination>
+    </el-row>
  </el-card>
 </template>
 
@@ -29,11 +44,23 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前选中的标签
-      list: [] // 接收素材数据
+      list: [], // 接收素材数据
+      page: {
+        total: 0,
+        pageSize: 8,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    // 点击分页时的方法
+    changePage (newPage) {
+      this.page.currentPage = newPage // 将最新获取到的页码给当前页
+      this.getMaterial() // 重新获取数据
+    },
+    // 切换选项时的方法
     changeTab () {
+      this.page.currentPage = 1 // 每次切换选项时，都将页码重置为1
       this.getMaterial()
     },
     // 获取素材
@@ -41,10 +68,13 @@ export default {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: this.activeName === 'collect'
+          collect: this.activeName === 'collect',
+          page: this.page.currentPage, // 页数
+          per_page: this.page.pageSize // 每页数量
         }
       }).then(res => {
         this.list = res.data.results
+        this.page.total = res.data.total_count
       })
     }
   },
