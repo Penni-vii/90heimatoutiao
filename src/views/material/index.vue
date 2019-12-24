def767010e855142ad5a8ad5f46922740a5f2013
Liwenxiu <742952713@qq.com>
2019-12-24 23:19:51 +0800

    黑马头条PC-素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,12 +1,24 @@
 <template>
  <el-card>
+   <!-- 头部内容 -->
    <bread-crumb slot="header">
     <template slot="title">
       素材管理
     </template>
    </bread-crumb>
-   <el-tabs v-model="activeName">
-     <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
+   <!-- 标签页选项卡 -->
+   <el-tabs v-model="activeName" @tab-click="changeTab">
+     <el-tab-pane label="全部图片" name="all">
+       <div class="hehe">
+         <el-card class="img-card" v-for="item in list" :key="item.id">
+           <img :src="item.url" alt="">
+           <el-row class="haha" type="flex" align="middle" justify="space-around">
+             <i class="el-icon-star-on"></i>
+             <i class="el-icon-delete-solid"></i>
+           </el-row>
+         </el-card>
+       </div>
+     </el-tab-pane>
      <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
    </el-tabs>
  </el-card>
@@ -16,12 +28,57 @@
 export default {
   data () {
     return {
-      activeName: 'all'
+      activeName: 'all', // 当前选中的标签
+      list: [] // 接收素材数据
     }
+  },
+  methods: {
+    changeTab () {
+      this.getMaterial()
+    },
+    // 获取素材
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect'
+        }
+      }).then(res => {
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.hehe {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 220px;
+    position: relative;
+    margin: 20px 50px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .haha {
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      font-size: 20px;
+      height: 36px;
+      background-color: #f4f5f6;
+      i {
+        cursor: pointer;
+      }
+    }
+  }
+}
 </style>
