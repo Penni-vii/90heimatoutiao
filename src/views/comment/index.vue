81488e96d136346e3257597c80f305a58aff2a14
Liwenxiu <742952713@qq.com>
2019-12-24 21:21:32 +0800

    黑马头条PC-评论列表-列表分页及请求以及加载状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 整个是一个卡片 -->
-  <el-card>
+  <el-card v-loading="loading">
     <!-- 用自己封装的面包屑组件 -->
     <bread-crumb slot="header">
       <template slot="title">
@@ -20,6 +20,16 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center" align="middle" style="height:80px">
+      <!-- 分页组件 -->
+      <el-pagination background layout="prev, pager, next"
+        :total="page.total"
+        :page-size="page.pageSize"
+        :current-page="page.currentPage"
+        @current-change="changePage"
+      >
+      </el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -27,19 +37,35 @@
 export default {
   data () {
     return {
-      list: [] // 定义一个空数组来接收请求回来的表格的数据
+      list: [], // 定义一个空数组来接收请求回来的表格的数据
+      loading: false,
+      page: {
+        total: 0,
+        pageSize: 10, // 默认一页显示10条数据
+        currentPage: 1 // 默认显示第一页
+      }
     }
   },
   methods: {
+    // 页码改变事件
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getComment() // 重新请求数据
+    },
+    // 获取评论
     getComment () {
+      this.loading = true // 先打开进度条
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(res => {
+        this.loading = false // 关闭进度条
         // console.log(res.data.results)
         this.list = res.data.results
+        this.page.total = res.data.total_count // 总条数
       })
     },
+    // 格式化布尔值
     fomatterBoolean (row, column, cellvalue, index) {
       return cellvalue ? '正常' : '关闭'
     },
