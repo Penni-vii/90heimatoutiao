6d0fba7a2481b0818fc43193a50b26bacd686353
Liwenxiu <742952713@qq.com>
2019-12-27 23:20:15 +0800

    黑马头条PC-发布文章-封面功能设计

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -15,7 +15,7 @@
       <quill-editor v-model="formData.content" style="height:300px"></quill-editor>
     </el-form-item>
     <el-form-item style="margin-top:120px" prop="cover" label="封面">
-      <el-radio-group v-model="formData.cover">
+      <el-radio-group @change="changeType" v-model="formData.cover.type">
         <!-- // 封面类型 -1:自动，0-无图，1-1张，3-3张 -->
         <el-radio :label="1">单图</el-radio>
         <el-radio :label="3">三图</el-radio>
@@ -23,6 +23,7 @@
         <el-radio :label="-1">自动</el-radio>
       </el-radio-group>
     </el-form-item>
+    <!-- {{formData.cover.images}} -->
     <el-form-item prop="channel_id" label="频道">
       <el-select v-model="formData.channel_id" placeholder="请选择">
         <el-option v-for="item in channel" :key="item.id" :label="item.name" :value="item.id"></el-option>
@@ -78,6 +79,16 @@ export default {
     }
   },
   methods: {
+    // 监听 封面类型的改变
+    changeType () {
+      if (this.formData.cover.type === 0 || this.formData.cover.type === -1) {
+        this.formData.cover.images = [] // 无图或者自动
+      } else if (this.formData.cover.type === 1) {
+        this.formData.cover.images = [''] // 一张图
+      } else if (this.formData.cover.type === 3) {
+        this.formData.cover.images = ['', '', ''] // 三张图
+      }
+    },
     // 发布文章
     publishArticle (draft) {
       this.$refs.form.validate(isOK => {
